8adb5c88686abdf3fbc312260880d1c57058c753
jonahsaltzman <jonahsaltzman@gmail.com>
2022-02-21 22:39:26 -0500

    add delete build function

--- src/components/BuildList.tsx
+++ src/components/BuildList.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Build } from "../classes/build";
 import { Form } from 'react-bootstrap'
-import { buildContext } from '../hooks/context/createContext'
+import { buildContext, authContext } from '../hooks/context/createContext'
 import { Actions } from "../declarations/enums";
 import { Spinner } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 const List = (props: ListProps) => {
     const {selected, dispatch} = useContext(buildContext)
+    const {auth} = useContext(authContext)
     const [buildName, setBuildName] = useState('')
     const [newBuild, setNewBuild] = useState(false)
 
@@ -24,8 +26,7 @@ const List = (props: ListProps) => {
 
     const editName = (e: React.FormEvent<HTMLFormElement>, buildId: number) => {
         e.preventDefault()
-        const found = props.builds.find(b => b.buildId === buildId)
-        const i = props.builds.indexOf(found)
+        const [found, i] = findBuild(buildId)
         found.buildName = buildName
         const newArr = [...props.builds]
         newArr[i] = Build.clone(found)
@@ -36,10 +37,10 @@ const List = (props: ListProps) => {
         setBuildName('')
     }
 
-    const nameForm = (isNew?: boolean, buildId?: number) => {
+    const NameForm = (isNew?: boolean, buildId?: number) => {
         const prevName = props.builds.find(
 					(b) => b.buildId === buildId
-				).buildName
+				).buildName ?? null
         if (!isNew) setBuildName(prevName)
         return (
 			<Form onBlur={() => {
@@ -66,10 +67,38 @@ const List = (props: ListProps) => {
     }
 
     const deleteBuild = (build: Build) => {
-        
+        build.delete(auth.token).then(res => {
+            if (res) {
+                const [, i] = findBuild(build.buildId)
+                const newArr = [...props.builds]
+                newArr.splice(i, 1)
+                props.setBuilds(newArr)
+                if (build.buildId === selected.buildId) {
+                    const newSelected = newArr.length
+                        ? newArr[i]
+                            ? newArr[i]
+                            : newArr[i - 1]
+                            ? newArr[i - 1]
+                            : newArr[i + 1]
+                            ? newArr[i + 1]
+                            : null
+                        : null
+                    dispatch({type: Actions.Swap, build: newSelected})
+                }
+            } else {
+                toast('Error deleting build')
+            }
+        })
+    }
+
+    const findBuild = (buildId: number): [Build, number] => {
+        const found = props.builds.find((b) => b.buildId === buildId)
+		const i = props.builds.indexOf(found)
+        return [found, i]
     }
 
     const newBuildLi = (<span className="new-build">New Build</span>)
+
     if (props.loading) {
         return (
             <Spinner animation='border' className="list-spinner" />
@@ -98,10 +127,10 @@ const List = (props: ListProps) => {
 							: null}
 						{props.authed ? (
 							<div
-								key={props.builds.length + 1 && -1}
+								key={props.builds.length + 1 ?? -1}
 								onClick={() => setNewBuild(true)}
 								className='build-li'>
-								{newBuild ? nameForm : newBuildLi}
+								{newBuild ? NameForm() : newBuildLi}
 							</div>
 						) : null}
 					</div>
